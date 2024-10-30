668931f0bb3457ce7db5e03900201d93954ba7ea
Mikita.Shah <mikitashah2009@gmail.com>
2024-10-30 22:40:48 +0530

    bring all components together, handle state management, and make the API request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { WeatherData } from './types/WeatherData';
+import axios from 'axios';
+
+import SearchBar from './components/SearchBar';
+import ErrorMessage from './components/ErrorMessage';
+import WeatherDisplay from './components/WeatherDisplay';
+import './App.css'
+
 
 function App() {
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSearch = async (city: string) => {
+    setError(null);
+    try {
+      const apiKey = 'c6861c716a085f252916274bbcc0a94d';
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+
+      const data = response.data;
+      const weatherData: WeatherData = {
+        city: data.name,
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        icon: data.weather[0].icon,
+      };
+      setWeather(weatherData);
+    } catch (error) {
+      setError("City not found. Please try again");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Dashboard</h1>
+      <SearchBar onSearch={handleSearch} />
+      {error && <ErrorMessage message={error} />}
+      {weather && < WeatherDisplay weather={weather} />}
     </div>
   );
 }
